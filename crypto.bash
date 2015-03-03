6ce54af08fcf13cacd74df17947f878ab10a4f5c
Louis T. Getterman IV <thad.getterman@gmail.com>
2015-03-02 21:54:53 -0600

    Crypto and Version
    Crypto - Beginning framework for conveniently interfacing to varying
    cryptographic libraries and hashes from a higher level and aiming to
    pull results from a higher level without concern of what's happening at
    a lower level.
    
    Version - More easily determine what version/information of GGCOM
    related libraries and applications (both Bash and Python) with
    continued path towards checking for updates.

---
+++ crypto.bash
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+#
+# GGCOM - Bash - Library - Cryptography v201503030304
+# Louis T. Getterman IV (@LTGIV)
+# www.GotGetLLC.com | www.opensour.cc/ggcom/lib/crypto
+#
+# Example usage:
+# source "crypto.bash"
+# echo "`cryptoHashCalc md5 string 'Hello World'`"
+# echo "`cryptoHashCalc sha1 file '/etc/hosts'`"
+
+# List Support
+function cryptoSupport() {
+#	echo "[OpenSSL File]"
+#	/usr/bin/openssl sha1 FILE;
+#	
+#	echo "[OpenSSL String]"
+#	cat FILE | /usr/bin/openssl sha1;
+#	
+#	echo "[Hash.Bash File]"
+#	./hash.bash sha1 FILE;
+#	
+#	echo "[Hash.Bash String]";
+#	cat FILE | ./hash.bash sha1
+} # END FUNCTION: CRYPTO SUPPORT
+
+# Calculate hash
+# Example usage:
+# echo "`cryptoHashCalc [md5|sha1|..] [string|file] '/etc/hosts'`"
+function cryptoHashCalc() {
+	local hash=${1-md5}
+	local method=${2-string}
+	local request=${3-''}
+	
+	local hashpath=''
+	local hashpostargs=''
+
+	#----- Find program, and arguments
+	if hash "${hash}sum" 2>/dev/null; then
+		hashpath="`type -p ${hash}sum`"
+		hashpostargs="cut -d' ' -f1"
+		
+	elif hash "${hash}" 2>/dev/null; then
+		hashpath="`type -p $hash`"
+		hashpostargs="cut -d' ' -f1"
+
+	elif hash gpg 2>/dev/null; then
+		hashpath="`type -p gpg` --print-md ${hash}"
+		hashpostargs="sed 's/ //g' | tr '[:upper:]' '[:lower:]' | tr -d '(\n|\r)'"
+
+	elif hash openssl 2>/dev/null; then
+		hashpath="`type -p openssl` dgst -${hash}"
+		hashpostargs='cat'
+
+	else
+		hashpath=''
+		hashargs=''
+
+	fi
+	#-----/Find program, and arguments
+
+	#----- Doesn't exist
+	if [ -z "$hashpath" ]; then
+		echo -n ''
+		exit 1;
+	fi
+	#-----/Doesn't exist
+
+	#----- Calculate Hash
+	case $method in
+		string)
+			echo -n "$request" | eval "$hashpath" | eval "$hashpostargs"
+			;;
+
+		file)
+			eval "$hashpath" <$request | eval "$hashpostargs"
+			;;
+	esac
+	#-----/Calculate Hash
+
+} # END FUNCTION: HASH CALCULATION
---
+++ version.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# GGCOM - Bash - Library - Version v201503030304
+# Louis T. Getterman IV (@LTGIV)
+# www.GotGetLLC.com | www.opensour.cc/ggcom/lib/version
+#
+# Example usage:
+# source "version.bash"
+# echo "`getVersion '/usr/bin/dfwu.py' [line|header|program|number]`"
+
+function getVersion() {
+	local path=${1-$0}
+	local type=${2-number}
+	
+	local versLine=`head -n5 "$1" | grep -n 'v[0-9]' | cut -f1 -d:`
+	local versHead=`tail -n+"$versLine" "$path" | head -n3 | sed 's/# *//g'`
+	local versProg=`echo "$versHead" | head -n1`
+	local versNum=`echo "$versHead" | grep -Eo 'v[0-9]{1,}' | grep -Eo '[0-9]{1,}'`
+	
+	local veret=''
+	
+	case $type in
+		line)
+			veret=$versLine
+			;;
+
+		header)
+			veret=$versHead
+			;;
+
+		program)
+			veret=$versProg
+			;;
+		
+		number)
+			veret=$versNum
+			;;
+		
+		*)
+			veret=$versNum
+			;;
+	esac
+	
+	echo "$veret"
+} # END FUNCTION
